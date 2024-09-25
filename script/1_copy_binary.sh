2a076c45280a2b2b2161910fa71761fbb454c83b
yumiel yoomee1313 <yumiel.ko@kaia.io>
2024-09-25 14:29:48 +0800

    update script

--- script/1_copy_binary.sh
+++ script/1_copy_binary.sh
@@ -1,11 +1,5 @@
-if [ $# -eq 0 ]; then
-  echo "Warning: no arguments"
-  exit 0
-fi
-# 지정해서 바꿔주기
-cd ~/project/kaia
-PROJECT_DIR=~/project/network-local
-cp build/bin/kcn ${PROJECT_DIR}/cn/bin/kcn_$1
-cp build/bin/kpn ${PROJECT_DIR}/pn/bin/kpn_$1
-cp build/bin/ken ${PROJECT_DIR}/en/bin/ken_$1
+# change the below paths to your path
+KAIA_DIR=~/workdir/kaia
+SCRIPT_DIR=~/workdir/kaia-rpc-tester/script
+cp $KAIA_DIR/build/bin/kcn ${SCRIPT_DIR}/cn/bin/kcn
 
